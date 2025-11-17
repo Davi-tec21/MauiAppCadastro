1d1bb4c365d60f2bd73e63e09eb09ca5afda712f
Davi-tec21 <daviforeignertriper@gmail.com>
2025-11-16 22:35:33 -0300

    send

--- MauiAppCadastro/Models/Eventocs.cs
+++ MauiAppCadastro/Models/Eventocs.cs
@@ -3,8 +3,8 @@
     public class Evento
     {
         public string Nome { get; set; }
-        public DateTime DataInicio { get; set; } = new DateTime(2025, 1, 1);  
-        public DateTime DataTermino { get; set; } = new DateTime(2025, 1, 1);
+        public DateTime DataInicio { get; set; } 
+        public DateTime DataTermino { get; set; }
         public int NumeroParticipantes { get; set; }
         public string Local { get; set; }
         public double CustoPorParticipante { get; set; }
